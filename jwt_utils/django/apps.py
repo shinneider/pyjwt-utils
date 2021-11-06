cec52d5033735b162e882089e530e979bce042c9
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2021-11-06 17:20:30 -0300

    fix apps import

--- jwt_utils/django/apps.py
+++ jwt_utils/django/apps.py
@@ -4,4 +4,5 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class DjangoJwtUtilsConfig(AppConfig):  # Our app config class
+    name = 'jwt_utils.django'
     verbose_name = _('Django JWT Utils')
