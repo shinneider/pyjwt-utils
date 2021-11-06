df532b89b080176b65ff7eb2342469091103da59
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2021-11-06 17:08:29 -0300

    fix version getter

--- jwt_utils/django/__init__.py
+++ jwt_utils/django/__init__.py
@@ -5,4 +5,4 @@ except ImportError:
     django = None
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
-    default_app_config = 'django_admin_inline_paginator.apps.DjangoAdminInlinePaginatorConfig'
+    default_app_config = 'jwt_utils.django.apps.DjangoAdminInlinePaginatorConfig'
